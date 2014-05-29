f1533605c9deaf8afeb753b6f306051c394cda33
Brady <bethomas@calpoly.edu>
2014-05-29 16:12:49 -0700

    Commiting non broken code

--- DynamicProgrammingSolution.java
+++ DynamicProgrammingSolution.java
@@ -16,11 +16,18 @@ public class DynamicProgrammingSolution {
       int table[][] = buildTable(items);
 
 
-      return solved;
+      return new ArrayList<Item>();
    }
 
    private int[][] buildTable(ArrayList<Item> items) {
-      int table[][] = new int[items.size() + 1][items.size() + 1];
+      int table[][] = new int[items.size() + 1][capacity + 1];
+
+      /*Initializes the base case for the table setting the 0 position in columns and rows to 0. */
+      for (int i = 0; i < capacity + 1; i++) {
+         if (i < items.size() + 1)
+            table[i][0] = 0;
+         table[0][i] = 0;
+      }
 
       return table;
    }
